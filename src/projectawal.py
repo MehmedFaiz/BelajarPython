14fe8957096f49502101cbfdb993516e75ccde4a
MohammadFaiz23 <madfaiz323@gmail.com>
2025-12-14 10:24:59 +0700

    Initial commit

---
+++ src/projectawal.py
@@ -0,0 +1,17 @@
+#print('Hello world!')
+#a = 5 * 2
+#print(a)
+
+#data_int = bool(int(input('masukkan angka: ')))
+#print('data =', data_int,',type =',type(data_int))
+
+for i in range(2):
+    print('check')
+
+for something in range(4):
+    print(something)
+
+print(7 + 3)
+print("aku mah apa")
+aku = "apis"
+print(aku)
