fbf81f4bcea1b659a2b1d12a69d6aced50779a1b
MohammadFaiz23 <madfaiz323@gmail.com>
2026-04-14 20:13:32 +0700

    first ML project and notebook setup

--- src/projectawal.py
+++ src/projectawal.py
@@ -5,13 +5,111 @@
 #data_int = bool(int(input('masukkan angka: ')))
 #print('data =', data_int,',type =',type(data_int))
 
-for i in range(2):
-    print('check')
+#for i in range(2):
+    #print('check')
 
-for something in range(4):
-    print(something)
+#for something in range(4):
+    #print(something)
 
-print(7 + 3)
-print("aku mah apa")
-aku = "apis"
-print(aku)
+#print(7 + 3)
+#print("aku mah apa")
+#aku = "apis"
+#print(aku)
+# ini untuk menunjukan komentar untuk penjelasan projek yang kita buat,
+# diusahakan setiap baris kode memiliki komentar untuk menjelaskan kodenya ya
+
+#kamu = input()
+#print(type(kamu + aku))
+
+#a = int(input())
+#print(type(a))
+
+# x = 13
+# y = "km"
+# print(str(x) + y)
+# y = 3.5
+# print(x + y)
+# print(type(y))
+# print(x == y)
+# a = (3 > 2) and False
+# b = False
+# print(a and b)
+# print(a or b)
+# for i in range(3):
+#     print(i)
+
+# i = 50
+# while i > 20:
+#     print("Alamak")
+#     i = i - 2
+# password = "SecretWord"
+# guess = input()
+# while guess != password:  
+#   guess = input() 
+# print("Access Granted")
+# ab = int(input())
+# if ab > 20:
+#     print("Youre welcome")
+# else:
+#     print("hayoo")
+
+# umur = int(input())
+# if 20 < umur <= 40:
+#     print("Udh gede")
+# elif umur > 40:
+#     print("Kegedean")
+# else:
+#     print("masi kecil")
+# print("Cek umur berhasil")
+# anti = ["aku","kamu","dia"]
+# anti[1] = "mereka"
+# print(anti[1])
+# namagunung = ["bromo","prau","slamet","merbabu"]
+# print(namagunung[0] + namagunung[1])
+# namagunung[3] = "sindoro"
+# print(namagunung[3])
+
+# nama = "atiqa zulva aulia"
+# prodi = "PTI"
+# ip = 3.9
+# hobi = "climbing"
+
+# identitas = [nama,prodi,ip,hobi]
+# simpan = int(input())
+# print(identitas[simpan])
+
+# list = ["burger","kebab","martabak","nasgor"]
+# list[2] = "putu"
+# print(list[2:4])
+
+# ada = "makanan"
+# print(ada[:4])
+
+# print(range(4))
+
+# print("how",3)
+# x = "NewYork"
+# print(x.upper())
+# print(x.lower())
+# print(x.capitalize())
+# print(x.find("e"))
+
+# x = "avenger"
+# print(len(x))
+# list = ["ikan","ayam","paus"]
+# list.append("bebek")
+# print(list)
+# list.insert(0,"burung")
+# print(list[4])
+# list.pop(1)
+# print(list)
+
+def how():
+    print("hayolo")
+
+how()
+def wow(a,b):
+    index = a*b
+    return index
+
+print(wow(4,5))
